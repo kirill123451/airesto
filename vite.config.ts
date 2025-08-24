14d383288301d3283f5e9a83791c80b911dae216
kirill <prostoskvasil@gmail.com>
2025-08-24 17:23:04 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/kirill123451/airesto',
+  base: '/airesto/',
   plugins: [
     vue(),
     vueDevTools(),
